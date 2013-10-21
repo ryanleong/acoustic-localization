697e76542b7e10e983e0b6ea6a1d534cf029bb52
Ani Fichadia <Ani.Fichadia@gmail.com>
2013-10-21 17:09:02 +1100

    Fixed commented code in GraphActivity

--- src/com/comp90017/teamA/assignment/Graph/GraphActivity.java
+++ src/com/comp90017/teamA/assignment/Graph/GraphActivity.java
@@ -17,16 +17,16 @@ public class GraphActivity extends Activity
 		super.onCreate (savedInstanceState);
 		setContentView (R.layout.activity_graph);
 
-		GraphView gv = (GraphView) findViewById (R.id.graphView);
-
-		// gv.resetGraph ();
-		// gv.addLandmarkEdge (GraphView.landmark1, GraphView.landmark2, 20);
-		// gv.addLandmarkEdge (GraphView.landmark2, GraphView.landmark3, 20);
-		// gv.addLandmarkEdge (GraphView.landmark3, GraphView.landmark1, 20);
+		// GraphView gv = (GraphView) findViewById (R.id.graphView);
 		//
-		// gv.addEmitterEdge (GraphView.landmark1, 5.682);
-		// gv.addEmitterEdge (GraphView.landmark2, 5.682);
-		// gv.addEmitterEdge (GraphView.landmark3, 5.682);
+		// gv.resetGraph ();
+		// gv.addLandmarkEdge (GraphView.landmark1, GraphView.landmark2, 2 * 10);
+		// gv.addLandmarkEdge (GraphView.landmark2, GraphView.landmark3, 2 * 10);
+		// gv.addLandmarkEdge (GraphView.landmark3, GraphView.landmark1, 2 * 10);
+		//
+		// gv.addEmitterEdge (GraphView.landmark1, 2 * 5.682);
+		// gv.addEmitterEdge (GraphView.landmark2, 2 * 5.682);
+		// gv.addEmitterEdge (GraphView.landmark3, 2 * 5.682);
 		//
 		// gv.invalidate ();
 	}
